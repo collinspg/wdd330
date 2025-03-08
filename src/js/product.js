ac3db62e5f8a91fa23648d0925fb342b42d1318d
Hyrummorales23 <hyrummorales23@gmail.com>
2025-03-08 12:26:01 -0600

    sync with remote main branch

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,8 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; 
-  cartItems.push(product); 
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
 
