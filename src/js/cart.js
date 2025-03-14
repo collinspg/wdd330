ec308a9cc937924347556b81567c4d0418f75542
Cindy Jane Vick <cindyjanevick@gmail.com>
2025-03-15 08:40:21 +0900

    fix version

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,6 +7,8 @@ function renderCartContents() {
 }
 
 function cartItemTemplate(item) {
+   console.log(item);
+   console.table(item);
   const colorName = item.Colors?.[0]?.ColorName || "No Color";
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
